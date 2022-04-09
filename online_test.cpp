e30832f26d1d8587f49225fa57a1720174d8440d
Sunny <sunny.kumar.it23@heritgeit.edu.in>
2022-04-10 01:43:31 +0530

    First Commit

---
+++ online_test.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll               long long
+#define ull              unsigned long long
+#define ff               first
+#define ss               second
+#define pb               push_back
+#define mp               make_pair
+#define pii              pair<int,int>
+#define vi               vector<int>
+#define vll              vector<ll> 
+#define vvi              vector< vector<int>>
+#define vvll             vector< vector<ll>>
+#define mii              map<int,int>
+#define pqb              priority_queue<int>
+#define pqs              priority_queue<int, vector<int>, greater<int>>
+#define setbits(x)       __builtin_popcountll(x)
+#define zrobits(x)       __builtin_ctzll(x)
+#define mod              1000000007
+#define inf              1e18
+#define ps(x,y)          fixed<<setprecision(y)<<x
+#define mk(arr, n, type) type *arr=new type[n];
+#define wt(x)            int x; cin>>x; while( x-- )
+#define rep(i,a,b)       for( int i=a; i<=b; i++ )
+#define repi(i,a,b)      for( int i=a; i>=b; i-- )
+#define sp               ' '
+#define nl               char(10)
+#define endl             char(10)
+#define PRT(ar)          for( auto i : ar ) cout<<i<<sp; cout<<nl;
+#define mems(x,ch)       memset(x,ch,sizeof(x))
+#define sortv(x)         sort(x.begin(),x.end())
+#define sortvr(x)        sort(x.rbegin(),x.rend())
+#define all(x)           x.begin(), x.end()
+
+const ll cnst = 1e5 + 5;
+
+void fastIO(){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+//    #ifndef ONLINE_JUDGE
+//        freopen("input.txt", "r", stdin);
+//        freopen("output.txt", "w", stdout);
+//    #endif
+}
+
+
+
+int32_t main(){
+    fastIO();
+
+
+    int x; 
+    cin>>x;
+    cout<<x;
+    
+    return 0;
+}
+    
+    
